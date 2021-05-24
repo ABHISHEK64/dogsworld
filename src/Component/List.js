0657b7cdec79f865c4ded9f84caaf24ab3ad5ec4
ABHISHEK64 <abhisheksachan27@gmail.com>
2021-05-24 09:40:01 +0530

    Done3

--- src/Component/List.js
+++ src/Component/List.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import  Modal  from '@material-ui/core/Modal';
 import ClearIcon from '@material-ui/icons/Clear'; 
+import Card from '@material-ui/core/Card';
+ 
+import CardContent from '@material-ui/core/CardContent'; 
+
 import './About.css';
 const List = (list) => {
     const [open, setOpen] =React.useState(false);
@@ -32,7 +36,12 @@ aria-describedby="simple-modal-description"
 </Modal>
 
 <div className="card_img">
-<img src={list.url}    className="card-img-top" alt={list.url} onClick={handleOpen}/>
+  <Card className='Card1'>
+    <CardContent>
+    <img src={list.url}    className="card-img-top" alt={list.url} onClick={handleOpen}/>
+    </CardContent>
+  </Card>
+ 
 </div>
 </>
     )
